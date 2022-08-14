364a9a81d6c2e71fce65907498f99126bcf1c7b2
Bill West <william.a.west@gmail.com>
2022-08-14 01:47:50 -0600

    Add result process stellarium view set action

--- astrometry.py
+++ astrometry.py
@@ -1,9 +1,51 @@
 import os
-from multiprocessing import Process
+from collections import namedtuple
+from multiprocessing import Process, Queue
 from subprocess import run, PIPE, STDOUT
 from time import time
 
 astrometry_install_dir = '/usr/local/astrometry'
+
+
+def solver(filename, location):
+    queue = Queue()
+
+    process = Process(target=_astrometry_solver, args=(filename, location, queue,))
+    process.start()
+    process.join()
+
+    solution = queue.get()
+    SolverSolution = namedtuple('SolverSolution', solution.keys())
+
+    return SolverSolution(**solution)
+
+
+def wcsinfo_parse(wcsinfo_output):
+    lines = wcsinfo_output.decode('UTF-8').splitlines()
+    solution = {}
+
+    for line in lines:
+        key, value = line.split(' ')
+        solution[key] = value
+
+    return solution
+
+
+def astrometry_to_stellarium_ra(astrometry_ra):
+    """ from: '01:54:54.240'
+          to: '00h 42m 44s'
+    """
+    hour, minute, second = astrometry_ra.split(':')
+    return f'{hour}h {minute}m {second}s'
+
+
+def astrometry_to_stellarium_dec(astrometry_dec):
+    """ from: '+62:44:30.621'
+          to: '41d 16m 9s'
+    """
+    degree, minute, second = astrometry_dec.split(':')
+    return f'{degree}d {minute}m {second}s'
+
 
 def _deactivate_venv():
     venv = os.environ.get('VIRTUAL_ENV', None)
@@ -18,13 +60,14 @@ def _deactivate_venv():
     # return original values
     return venv, path
 
+
 def _reactivate_venv(venv, path):
     if venv:
         os.environ['VIRTUAL_ENV'] = venv
     os.environ['PATH'] = path
 
 
-def _astrometry_solver(filename, location):
+def _astrometry_solver(filename, location, queue):
     # TODO radius should probably be based on oculars settings/fov,
     #      though it's partially affected by operator skill; for now,
     #      just make it static + conservative
@@ -41,23 +84,29 @@ def _astrometry_solver(filename, location):
             '--radius', radius_deg,
             filename,
     ]
-    exec_and_args = [solver_bin] + solver_args
-    exec_string = ' '.join(exec_and_args)
+    solver_exec_and_args = [solver_bin] + solver_args
+    solver_exec_string = ' '.join(solver_exec_and_args)
+
+    wcsinfo_bin = f'{astrometry_install_dir}/bin/wcsinfo'
+    wcs_file_name = '.'.join(filename.split('.')[0:-1]) + '.wcs'
+    wcsinfo_args = [wcs_file_name]
+    wcsinfo_exec_and_args = [wcsinfo_bin] + wcsinfo_args
+    wcsinfo_exec_string = ' '.join(wcsinfo_exec_and_args)
 
     # deactivate venv temporarily
     venv, path = _deactivate_venv()
 
-    print(f'executing: {exec_string}')
+    print(f'executing: {solver_exec_string}')
     solve_start = time()
-    run(exec_and_args, stdout=PIPE, stderr=STDOUT)
+    solver_result = run(solver_exec_and_args, stdout=PIPE, stderr=STDOUT)
     solve_finish = time()
     print(f'solver halt after {solve_finish - solve_start} sec')
+
+    print(f'executing: {wcsinfo_exec_string}')
+    wcsinfo_result = run(wcsinfo_exec_and_args, stdout=PIPE, stderr=STDOUT)
+    parsed_wcsinfo = wcsinfo_parse(wcsinfo_result.stdout)
 
     # reactivate venv
     _reactivate_venv(venv, path)
 
-
-def solver(filename, location):
-    p = Process(target=_astrometry_solver, args=(filename, location,))
-    p.start()
-    p.join()
+    queue.put(parsed_wcsinfo)
--- solve.py
+++ solve.py
@@ -1,6 +1,6 @@
 from ssc import current_location, snap_to_solver
 from capture import poll_for_file
-from astrometry import solver
+from astrometry import solver, astrometry_to_stellarium_ra, astrometry_to_stellarium_dec
 
 """get current view location, which can be used to narrow 
    down the plate solver's search task to a smaller set
@@ -17,9 +17,21 @@ print(f'found file: {image_path}')
 
 """next, run the plate solver"""
 solution = solver(image_path, location)
-print(f'solution result: {solution}')
+print(f'solution result: '
+      f'ra={solution.ra_center_hms}, '
+      f'dec={solution.dec_center_dms}, '
+      f'orientation={solution.orientation_center}, '
+      f'zoom={solution.merczoom}, '
+)
 
+args = {
+    'ra': astrometry_to_stellarium_ra(solution.ra_center_hms),
+    'dec': astrometry_to_stellarium_dec(solution.dec_center_dms),
+    'rotation_angle': solution.orientation_center,
+    'zoom_arc_min': solution.merczoom,
+}
 
 """finally: if a solution was found, reset the stellarium view and
    rotation based on the solver results"""
-#print(snap_to_solver())
+snap_result = snap_to_solver(args)
+print(f'snap_result = {snap_result}')
